e88c2acd5b0c1e7b500649f670c805efe46af764
rodnick5 <69417416+rodnick5@users.noreply.github.com>
2020-10-26 21:21:39 +0200

    button added

---
+++ javascript/script.js
@@ -0,0 +1,39 @@
+let indiv = document.getElementById('individual');
+let comp = document.getElementById('company');
+let indivBlock = document.getElementById('individual_block');
+let compBlock = document.getElementById('company_block')
+comp.onclick = function(){
+    compBlock.classList.add('active-btn');
+    comp.classList.add('active-text');
+    indivBlock.classList.remove('active-btn');
+    indiv.classList.remove('active-text');
+}
+indiv.onclick = function(){
+    indivBlock.classList.add('active-btn');
+    indiv.classList.add('active-text');
+    compBlock.classList.remove('active-btn');
+    comp.classList.remove('active-text');
+}
+    function trackScroll() {
+      var scrolled = window.pageYOffset;
+      var coords = document.documentElement.clientHeight;
+  
+      if (scrolled > coords) {
+        goTopBtn.classList.add('back_to_top-show');
+      }
+      if (scrolled < coords) {
+        goTopBtn.classList.remove('back_to_top-show');
+      }
+    }
+  
+    function backToTop() {
+      if (window.pageYOffset > 0) {
+        window.scrollBy(0, -30);
+        setTimeout(backToTop, 0);
+      }
+    }
+  
+    var goTopBtn = document.querySelector('.back_to_top');
+  
+    window.addEventListener('scroll', trackScroll);
+    goTopBtn.addEventListener('click', backToTop);

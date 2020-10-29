41667cea9e2a27b5011304516153fc6cf375ee65
rodnick5 <69417416+rodnick5@users.noreply.github.com>
2020-10-30 01:05:22 +0200

    smooth anim anchors added

--- javascript/script.js
+++ javascript/script.js
@@ -54,3 +54,15 @@ indiv.onclick = function(){
     window.addEventListener('scroll', trackScroll);
     goTopBtn.addEventListener('click', backToTop);
     window.addEventListener('scroll', navScroll);
+    
+    const anchors = document.querySelectorAll('a[href*="#"]');
+for ( let anchor of anchors){
+    anchor.addEventListener('click' , function(event){
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href');
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
